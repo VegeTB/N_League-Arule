108043ef397cb4c1d90a955a5c196fb8e410478e
VegeTB <mrlixiaocai@gmail.com>
2026-06-30 00:58:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ class MahjongPluginA(Star):
         }
         
         yield event.plain_result(
-            f"🅰️ A规对局 #{match_id} 已建立！\n"
+            f"对局 #{match_id} 已建立！\n"
+            f"规则：联盟A规\n"
             f"选手 {user_name} 已加入！ (1/4)\n"
             f"请其他选手发送 /a加入对局 加入。\n"
             f"(多桌同开时请输入 /a加入对局 {match_id} 加入本桌)"
@@ -286,7 +287,7 @@ class MahjongPluginA(Star):
         
         yield event.plain_result("\n".join(result_msg))
 
-    @command("a_mj_chombo", alias=["a错和", "A错和", "a罚分", "a_chombo"])
+    @command("a_mj_chombo", alias=["a错和", "A错和", "a罚分", "achombo"])
     async def chombo_a(self, event: AstrMessageEvent):
         """
         A规错和处罚：扣除指定用户 20pt
